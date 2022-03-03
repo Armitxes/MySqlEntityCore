ad17626418cabc1a2fe2aee6505e45f9c1625bc0
Jan Brodersen (Armitxes) <4rmitxes@gmail.com>
2022-03-03 18:36:55 +0100

    Move Model.Get method to the core for non default type models

--- Template/Core.cs
+++ Template/Core.cs
@@ -60,5 +60,55 @@ namespace MySqlEntityCore.Template {
             }
             this.Origin = this.MemberwiseClone();
         }
+
+        /// <summary>Get a list of records by the given conditions. Leave null/0 for all lines.</summary>
+        /// <param name="where">SQL "WHERE" condition</param>
+        /// <param name="orderby">SQL "ORDER BY" statement</param>
+        /// <param name="offset">Result offset</param>
+        /// <param name="limit">Result limit</param>
+        /// <returns></returns>
+        public static List<T> Get<T>(
+            string where=null,
+            string orderby=null,
+            uint offset=0,
+            uint limit=0
+        ) {
+            System.Type tType = typeof(T);
+            List<T> records = new List<T>();
+            ModelAttribute tTypeAttr = ModelAttribute.Get(tType);
+
+            string sql = $"SELECT {tTypeAttr.SqlFieldList} FROM {tTypeAttr.Table}";
+            if (where != null)
+                sql += $" WHERE {where}";
+            if (orderby != null)
+                sql += $" ORDER BY {orderby}";
+            if (offset > 0 || limit > 0) {
+                sql += $" LIMIT";
+                if (offset > 0 && limit > 0) 
+                    sql += $" {offset},{limit}";
+                else if (limit > 0)
+                    sql += $" {limit}";
+            }
+            sql += ";";
+                
+
+            List<Dictionary<string, object>> query = new Connection().Query(sql, true);
+            ConstructorInfo ctor = tType.GetConstructor(System.Type.EmptyTypes);
+            if (
+                ctor == null
+                || query.Count == 0
+            )
+                return records;
+
+            MethodInfo construct = tType.GetMethod("ConstructFromDictionary", new[] { query[0].GetType() });
+
+            foreach (Dictionary<string, object> entry in query)
+            {
+                object objRecord = ctor.Invoke(System.Type.EmptyTypes);
+                construct.Invoke(objRecord, new object[] { entry });
+                records.Add((T)System.Convert.ChangeType(objRecord, tType));
+            }
+            return records;
+        }
 	}
 }
