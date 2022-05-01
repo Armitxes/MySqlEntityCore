202ffb1ee633fb19e71e7e51ea4fe06b9745d945
Jan Brodersen (Armitxes) <4rmitxes@gmail.com>
2022-05-01 19:44:22 +0200

    ValueAsString: Null Check

--- Attribute/Field.cs
+++ Attribute/Field.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace MySqlEntityCore {
+namespace MySqlEntityCore
+{
 
-	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
-	public class FieldAttribute : Attribute {
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class FieldAttribute : Attribute
+    {
         public string Column { get; set; }
         public string Name { get; private set; }
         public uint Size { get; set; }
@@ -30,22 +32,26 @@ namespace MySqlEntityCore {
         public ModelAttribute Model { get; private set; }
         public PropertyInfo PropInfo { get; private set; }
 
-        private string StrSize {
-            get {
+        private string StrSize
+        {
+            get
+            {
                 if (Size == 0)
                     return "";
                 return Size.ToString();
             }
         }
 
-        public bool IsModelClass {
-            get {
-               return PropInfo.PropertyType.IsSubclassOf(typeof(Template.DefaultModel));
+        public bool IsModelClass
+        {
+            get
+            {
+                return PropInfo.PropertyType.IsSubclassOf(typeof(Template.DefaultModel));
             }
         }
 
-		internal static FieldAttribute Get(PropertyInfo property)
-		{
+        internal static FieldAttribute Get(PropertyInfo property)
+        {
             FieldAttribute field = property.GetCustomAttribute<FieldAttribute>();
             if (field == null)
                 return null;
@@ -56,16 +62,19 @@ namespace MySqlEntityCore {
             if (field.PrimaryKey)
                 field.Required = true;
 
-            if (field.AutoIncrement) {
+            if (field.AutoIncrement)
+            {
                 field.Required = true;
                 field.Unique = true;
             }
 
-			return field;
-		}
+            return field;
+        }
 
         internal string ValueAsString(object obj)
         {
+            if (obj == null)
+                return null;
             object value = PropInfo.GetValue(obj);
             if (value is DateTime)
                 return Convert.ToDateTime(value).ToString("yyyy-MM-dd HH:mm:ss");
@@ -74,13 +83,13 @@ namespace MySqlEntityCore {
             return value.ToString();
         }
 
-		internal string SqlCreate()
-		{
-			string[] strings = new string[] {
-				"`"+Column+"`", SqlType(), SqlAutoIncrement(), SqlRequired(), SqlUnique()
-			};
-			return string.Join(" ", strings.Where(str => str != ""));
-		}
+        internal string SqlCreate()
+        {
+            string[] strings = new string[] {
+                "`"+Column+"`", SqlType(), SqlAutoIncrement(), SqlRequired(), SqlUnique()
+            };
+            return string.Join(" ", strings.Where(str => str != ""));
+        }
 
         internal string SqlAlter(ModelAttribute model)
         {
@@ -88,17 +97,17 @@ namespace MySqlEntityCore {
             Dictionary<string, object> fieldInfo = model.DbFieldInfo(Column);
             if (fieldInfo == null)
                 return SqlAlterAddColumn();
-            
-			bool tblRequired = fieldInfo["IS_NULLABLE"].ToString() == "NO";
-			bool tblAutoIncrement = fieldInfo["EXTRA"].ToString().Contains("auto_increment");
+
+            bool tblRequired = fieldInfo["IS_NULLABLE"].ToString() == "NO";
+            bool tblAutoIncrement = fieldInfo["EXTRA"].ToString().Contains("auto_increment");
             string size = fieldInfo["CHARACTER_MAXIMUM_LENGTH"].ToString();
             if (size == "")
                 size = "0";
-			if (
-				Required == tblRequired
-				&& Size.ToString() == size
-				&& AutoIncrement == tblAutoIncrement
-			)
+            if (
+                Required == tblRequired
+                && Size.ToString() == size
+                && AutoIncrement == tblAutoIncrement
+            )
                 return "";
 
             string sql = SqlAlterModify() + ";";
@@ -116,7 +125,7 @@ namespace MySqlEntityCore {
         }
 
         ///<summary>Return the required sql to modify the current model field</summary>
-        public string SqlAlterModify(bool sqlShort=false)
+        public string SqlAlterModify(bool sqlShort = false)
         {
             if (sqlShort)
                 return $"MODIFY `{Column}` {SqlType()} {SqlAutoIncrement()} {SqlRequired()}";
@@ -124,46 +133,46 @@ namespace MySqlEntityCore {
         }
 
         ///<summary>Return the required SQL to drop auto increment</summary>
-        public string SqlDropAutoIncrement(bool sqlShort=false)
+        public string SqlDropAutoIncrement(bool sqlShort = false)
         {
             if (sqlShort)
                 return $"MODIFY `{Column}` {SqlType()} {SqlRequired()}";
             return $"ALTER TABLE `{Model.Table}` MODIFY `{Column}` {SqlType()} {SqlRequired()}";
         }
 
-		///<summary>Return MySQL compatible data type for a C# data type.</summary>
-		public string SqlType()
-		{
-			string typeName = PropInfo.PropertyType.Name.ToLower();
-			if (typeName == "string" && Size > 8000)
-				return "TEXT";
+        ///<summary>Return MySQL compatible data type for a C# data type.</summary>
+        public string SqlType()
+        {
+            string typeName = PropInfo.PropertyType.Name.ToLower();
+            if (typeName == "string" && Size > 8000)
+                return "TEXT";
 
             if (IsModelClass)
                 return "INT UNSIGNED";
 
-			return typeName switch
-			{
-				"string" => $"VARCHAR({Size})",
-				"int64" => "BIGINT",
-				"int32" => "INT",
-				"int16" => "SMALLINT",
-				"uint64" => "BIGINT UNSIGNED",
-				"uint32" => "INT UNSIGNED",
-				"uint16" => "SMALLINT UNSIGNED",
-				"decimal" => $"DECIMAL({Size}, {Decimal})",
-				"double" => $"DOUBLE({Size}, {Decimal})",
-				"single" => $"FLOAT({Size}, {Decimal})",
-				"char" => "CHAR(1)",
+            return typeName switch
+            {
+                "string" => $"VARCHAR({Size})",
+                "int64" => "BIGINT",
+                "int32" => "INT",
+                "int16" => "SMALLINT",
+                "uint64" => "BIGINT UNSIGNED",
+                "uint32" => "INT UNSIGNED",
+                "uint16" => "SMALLINT UNSIGNED",
+                "decimal" => $"DECIMAL({Size}, {Decimal})",
+                "double" => $"DOUBLE({Size}, {Decimal})",
+                "single" => $"FLOAT({Size}, {Decimal})",
+                "char" => "CHAR(1)",
                 "boolean" => "CHAR(0)",
-				"datetime" => "DATETIME",
-				_ => "",
-			};
-		}
+                "datetime" => "DATETIME",
+                _ => "",
+            };
+        }
 
-		public string SqlAutoIncrement() => AutoIncrement ? "AUTO_INCREMENT" : "";
+        public string SqlAutoIncrement() => AutoIncrement ? "AUTO_INCREMENT" : "";
 
-		public string SqlRequired() => Required ? "NOT NULL" : "NULL";
+        public string SqlRequired() => Required ? "NOT NULL" : "NULL";
 
-		public string SqlUnique() => Unique ? "UNIQUE KEY" : "";
-	}
+        public string SqlUnique() => Unique ? "UNIQUE KEY" : "";
+    }
 }
